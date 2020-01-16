9fe207b7423dee20fdf8ca007bcb4f081328e070
Caleb Harrison <CHarrison0127@gmail.com>
2020-01-16 10:43:38 -0600

    Initial Commit

---
+++ SAR_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SAR_iOS
+//
+//  Created by Caleb Harrison on 1/16/20.
+//  Copyright © 2020 Caleb Harrison. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
