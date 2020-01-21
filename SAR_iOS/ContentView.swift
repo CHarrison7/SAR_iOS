9646276a07944447bd81cd1c07e581447b7e2fb5
Travis Tomer <tmtomer@uh.edu>
2020-01-21 12:23:09 -0600

    Add files via upload

---
+++ SAR_iOS/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  SAR_iOS
+//
+//  Created by Caleb Harrison, Travis Tomer, Son Doan, and Larry Lu on 1/16/20.
+//  Copyright © 2020 Caleb Harrison. All rights reserved.
+//
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Login to DJI SDK")
+                .font(.title)
+                .bold()
+            HStack {
+                Button(action: {
+                    // What to perform
+                    print("Logon success!")
+                }) {
+                    // How the button looks like
+                    Text("Login")
+                        .font(.title)
+                        .bold()
+                        .padding()
+                }
+                
+                Button(action: {
+                    // What to perform
+                    print("Logout success!")
+                }) {
+                    // How the button looks like
+                    Text("Logoff")
+                        .font(.title)
+                        .padding()
+                    
+                        
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
