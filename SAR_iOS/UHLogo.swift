7d0a2f7046fb332df66bf364604f4a4f6bab8539
Travis Tomer <tmtomer@uh.edu>
2020-01-21 13:10:13 -0600

    Add files via upload

---
+++ SAR_iOS/UHLogo.swift
@@ -0,0 +1,21 @@
+//
+//  UHLogo.swift
+//  SAR_iOS
+//
+//  Created by Travis Tomer on 1/21/20.
+//  Copyright © 2020 Caleb Harrison. All rights reserved.
+//
+
+import SwiftUI
+
+struct UHLogo: View {
+    var body: some View {
+        Image("UH")
+    }
+}
+
+struct UHLogo_Previews: PreviewProvider {
+    static var previews: some View {
+        UHLogo()
+    }
+}
